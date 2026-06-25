a73ad918a639abeaddd53fe90a15f9ab0d3366ff
salangane281 <salangane281@gmail.com>
2026-06-25 08:56:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ client.on('interactionCreate', async interaction => {
 📸 Vui lòng upload ảnh hóa đơn/chứng từ ngay bên dưới tin nhắn này.`,
     });
     await fetch(
-  'https://n8n-zbeos5oumjfdy4pattr3i5cv.45.76.163.125.sslip.io/webhook/6e15bedf-5899-4851-a862-f6c2e6d81e02',
+  'http://n8n-zbeos5oumjfdy4pattr3i5cv:5678/webhook/6e15bedf-5899-4851-a862-f6c2e6d81e02',
   {
     method: 'POST',
     headers: {
